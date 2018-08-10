1f4c05be5d3c74b730a68883d826b8e3da8db0ce
Thibault Pelloquin <tiboqorl@yahoo.fr>
2018-08-10 17:24:23 +0200

    BirthDateTrait - Additional functions

--- lib/MaBeTiZ/Entity/Fragment/BirthDateTrait.php
+++ lib/MaBeTiZ/Entity/Fragment/BirthDateTrait.php
@@ -2,7 +2,9 @@
 
 namespace MaBeTiZ\Entity\Fragment;
 
+use DateInterval;
 use DateTime;
+use Exception;
 
 /**
  * Trait BirthDateTrait
@@ -32,5 +34,40 @@ trait BirthDateTrait
         $this->birthDate = $birthDate;
 
         return $this;
+    }
+
+    /**
+     * Additional functions
+     */
+
+    /**
+     * @return DateInterval|null
+     */
+    public function getAge(): ?DateInterval
+    {
+        if (is_a($this->birthDate, 'DateTime')) {
+            return $this->birthDate->diff(new DateTime('now'));
+        }
+
+        return null;
+    }
+
+    /**
+     * @param DateTime|null $date If null, checking for today.
+     * @return bool|null bool(true): "yes", bool(false): "no", null: "don't know".
+     */
+    public function isBirthday(?DateTime $date): ?bool
+    {
+        if (is_null($date)) {
+            $date = new DateTime('now');
+        }
+
+        if (is_a($this->birthDate, 'DateTime')) {
+            return
+                $this->birthDate->format('d') === $date->format('d')
+                && $this->birthDate->format('m') === $date->format('m');
+        }
+
+        return null;
     }
 }
